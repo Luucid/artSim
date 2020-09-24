1d654222296d31739c11bd34d5fae24a23eca115
lucid <54083739+Luucid@users.noreply.github.com>
2020-09-24 21:02:12 +0200

    cooler print, added teleport if stuck.

--- artSim.py
+++ artSim.py
@@ -23,39 +23,41 @@ class Draw():
     def __next__(self):
         
         self.chk = 1 
+        tpc = 1
        
-        #while(self.chk): 
         a = np.random.choice(self.ss, 2)     
         self.nextPos = self.curPos + a 
         
         while((a[0] == 0 and a[1] == 0) 
-              or (self.nextPos[0] >= d or self.nextPos[1] >= d) 
-              or (self.nextPos[0] <= 0 or self.nextPos[1] <= 0) 
+              or (self.nextPos[0] >= d) 
+              or (self.nextPos[0] <= 0) 
               or (self.lastX == int(a[0]) and self.lastY == int(a[1])) 
-              or self.isStuck(int(self.nextPos[0]-1),self.nextPos[1])
+              or self.isStuck(int(self.nextPos[0]),int(self.nextPos[1]))
               ):
-           
+            
+            
+            #print(tpc)
+            if(tpc > 5):
+                self.ss = [-tpc, 0, tpc]
+                
             a = np.random.choice(self.ss, 2)   
             self.nextPos = self.curPos + a 
+            tpc += 1
+        
             
         self.chk = self.checkEdges(tuple(self.nextPos), self.curPos)
-        
-        
-        # if (self.isStuck(int(self.nextPos[0]-1),self.nextPos[1])):
-        #     return 1
-        
-       
+            
+        self.ss = [-1, 0, 1]
         if(self.chk == 0):
             self.edgeX = np.append(self.edgeX, int(self.curPos[0]))   
             self.edgeY = np.append(self.edgeY, int(self.curPos[1]))  
             self.lastX = int(a[0])
             self.lastY = int(a[1])
             self.curPos = self.nextPos
-        
+            return 1
         return 0
-       
         
-            
+     
 
    
     
@@ -85,7 +87,7 @@ class Draw():
             if(y+1 in self.usedPoints[x-1] and y+1 in self.usedPoints[x] and y+1 in self.usedPoints[x+1]): #top
                 if(y in self.usedPoints[x-1] and y in self.usedPoints[x+1]): #mid
                     if(y-1 in self.usedPoints[x-1] and y-1 in self.usedPoints[x] and y-1 in self.usedPoints[x+1]): #bot
-                        print("stuck in middle")
+                       # print("stuck in middle")
                         return 1
                     
         ###################################################
@@ -163,10 +165,6 @@ class Draw():
         return(len(self.edgeX))
     
     def addPoint(self, x, y, pp): #pp = prev point
-        l = 0 #left
-        r = 0 #right
-        t = 0 #top
-        b = 0 #bot
     
         if(y in self.usedPoints[x]): #is spot empty
             return 1
@@ -183,14 +181,14 @@ class Draw():
                 if(self.diagCheck(x-1, y, x, y+1, pp, (x, y))): #c3
                     return 1
         
-        # if(x < self.d):
-        #     if(y > 0):
-        #         if(self.diagCheck(x, y-1, x+1, y, pp, (x, y))): #c1
-        #             return 1
+        if(x < self.d):
+            if(y > 0):
+                if(self.diagCheck(x, y-1, x+1, y, pp, (x, y))): #c1
+                    return 1
             
-        #     if(y < self.d):
-        #         if(self.diagCheck(x+1, y, x, y+1, pp, (x, y))): #c2
-        #             return 1
+            if(y < self.d):
+                if(self.diagCheck(x+1, y, x, y+1, pp, (x, y))): #c2
+                    return 1
         #############################################  
             
         self.usedPoints[x] = np.append(self.usedPoints[x], y)
@@ -210,17 +208,20 @@ class Draw():
 
 n = 1000
 d = n*2
-max_i = n*10
+prct = n / 10 #for status..
+
 i = 0
+
 start = ti.time_ns()
 
 art = Draw(n, d)
-# for i in range(n):
-#     print(i)
+
     
-while(art.getSizeX() < n and i < max_i):
-    next(art)
-    i+=1
+while(i < n):
+    i+=next(art)
+    if(i%prct == 0):
+        print("%d of %d"%(i, n))
+       
     
     
     
@@ -235,73 +236,24 @@ y = art.getEdgeY()
 
 print("size of x = %d, size of y = %d" % (len(x), len(y)))
 
-
     
 
 plt.figure(dpi=1200) 
 plt.axis("equal")
-plt.plot(x, y, "b-",linewidth=0.5)
+for i in range(1, n):
+    if(x[i] < x[i-1]):
+        if(x[i-1] - x[i] > 1):
+            plt.plot([x[i-1], x[i]], [y[i-1], y[i]], "b.")
+        else:
+            plt.plot([x[i-1], x[i]], [y[i-1], y[i]], "g-")
+            
+    else:
+        if(x[i] - x[i-1] > 1):
+            plt.plot([x[i-1], x[i]], [y[i-1], y[i]], "b.")
+        else:
+            plt.plot([x[i-1], x[i]], [y[i-1], y[i]], "r-")
+        
+            
+    
 plt.show()
-
-############################################
-        
-
-       # if(y in self.usedPoints[x]): #is spot empty
-       #      return 1
-
-        
-       #  if(x>0):
-       #      if(y > 0):
-       #          if(y in self.usedPoints[x-1] 
-       #             and y-1 in self.usedPoints[x] 
-       #             and y-1 in self.usedPoints[x-1] 
-       #             and pp == pp4
-       #             ): return 1 # c4
-              
-       #      if(y < self.d):
-       #          if(y in self.usedPoints[x-1] 
-       #             and y+1 in self.usedPoints[x] 
-       #             and y+1 in self.usedPoints[x-1] 
-       #             and pp == pp3
-       #             ): return 1 # c3
-                
-            
-       #      if(y in self.usedPoints[x-1]):
-       #          l = 1
-        
-                
-       #  if(x<self.d):
-            
-       #      if(y < self.d):
-       #          if(y in self.usedPoints[x+1] 
-       #             and y+1 in self.usedPoints[x] 
-       #             and y+1 in self.usedPoints[x+1] 
-       #             and pp == pp2
-       #             ): return 1 # c2
-                
-       #      if(y > 0):
-       #          if(y in self.usedPoints[x+1] 
-       #             and y-1 in self.usedPoints[x] 
-       #             and y-1 in self.usedPoints[x+1] 
-       #             and pp == pp1
-       #             ): return 1 # c1
-                
-                
-       
-       #  if(x < self.d):
-       #      if(y in self.usedPoints[x+1]):
-       #          r = 1      
-                
-       #  if(y < self.d):
-       #      if(y+1 in self.usedPoints[x]):
-       #          t = 1
-        
-       #  if(y > 0):
-       #      if(y-1 in self.usedPoints[x]):
-       #          b = 1
-        
-            
-        
-        
-       #  if(l and r and t and b):
-       #      return 1
+    
